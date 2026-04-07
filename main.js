c0ced401fe48978a0636eb9079c2efd7473c311a
sublomya-cell <sublomya@gmail.com>
2026-04-07 15:46:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -278,7 +278,7 @@ async function connectionUpdate(update) {
 ◦ *المنصة*: ${os.platform()} ${os.release()}
 ◦ *جهاز*: ${deviceName}
 ◦ *اسم البوت*: ${global.namebot}
-◦ *الوقت المتصل*: ${new Date().toLocaleString()}\n\n قناتي على الواتساب للمزيد من المعلومات \nhttps://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A`;
+◦ *الوقت المتصل*: ${new Date().toLocaleString()}`;
         
         this.sendMessage(global.nomerown + `@s.whatsapp.net`, {
             text: message
